c37871fe1b200b9cb9b8723bfbda2125ae050c5d
Royarunimb12 <69623012+Royarunimb12@users.noreply.github.com>
2020-08-25 10:07:30 +0530

    Create testColorToNumber.js

---
+++ testColorToNumber.js
@@ -0,0 +1,9 @@
+var GetPairNumberFromColors=require("./GetPairNumberFromColors.js");
+
+function testColorToNumber(majorColor, minorColor, expectedNumber) {
+    const pairNumber = GetPairNumberFromColors(majorColor, minorColor);
+    console.log(`${majorColor} ${minorColor} = ${pairNumber}`);
+    expect(pairNumber).to.equal(expectedNumber);
+}
+
+module.exports=testColorToNumber;
