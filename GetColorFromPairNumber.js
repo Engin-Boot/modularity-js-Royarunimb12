fe062c031cfed5c1a10caf5afe1293c165b228c3
Royarunimb12 <69623012+Royarunimb12@users.noreply.github.com>
2020-08-25 09:36:29 +0530

    Update GetColorFromPairNumber.js

--- GetColorFromPairNumber.js
+++ GetColorFromPairNumber.js
@@ -1,2 +1,14 @@
 var Major=require("./Colors.js").Major;
 var Minor=require("./Colors.js").Minor;
+
+function GetColorFromPairNumber(pairNumber) {
+    let colorPair = {};
+    const zeroBasedPairNumber = pairNumber - 1;
+    const majorColorIndex =
+        Math.floor(zeroBasedPairNumber / MinorColors.length);
+    colorPair.major = MajorColors[majorColorIndex];
+    colorPair.minor = MinorColors[zeroBasedPairNumber % MinorColors.length];
+    return colorPair
+}
+
+module.exports=GetColorFromPairNumber;
