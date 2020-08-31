2670a965cfea476967182a1b9ab696db82b59532
Royarunimb12 <69623012+Royarunimb12@users.noreply.github.com>
2020-08-31 12:57:43 +0530

    Update GetPairNumberFromColors.js

--- GetPairNumberFromColors.js
+++ GetPairNumberFromColors.js
@@ -1,19 +1,30 @@
 var MajorColors=require("./Colors.js").MajorColors;
 var MinorColors=require("./Colors.js").MinorColors;
 
-function GetPairNumberFromColors(majorColor, minorColor) {
+function getMajorIndex(majorColor){
     let majorIndex;
-    let minorIndex;
     for(majorIndex = MajorColors.length; majorIndex >= 0; majorIndex--) {
         if(MajorColors[majorIndex] == majorColor) {
             break;
         }
     }
+    return majorIndex;
+       
+}
+
+function getMinorIndex(minorColor){
+    let minorIndex;
     for(minorIndex = MinorColors.length; minorIndex >= 0; minorIndex--) {
         if(MinorColors[minorIndex] == minorColor) {
             break;
         }
     }
+    return minorIndex;
+}
+
+function GetPairNumberFromColors(majorColor, minorColor) {
+    let majorIndex=getMajorIndex(majorColor);
+    let minorIndex=getMinorIndex(minorColor);
     return majorIndex * MinorColors.length + minorIndex + 1;
 }
 
